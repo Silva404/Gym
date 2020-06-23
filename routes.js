23b9f058b97bfbe3c242d24d5a56298d0bc3ce38
Silva404 <brenosilva0252@gmail.com>
2020-06-23 16:24:56 -0300

    autentication started

--- routes.js
+++ routes.js
@@ -9,6 +9,19 @@ routes.get('/instructors', (req, res) => {
     return res.render('instructors/index')
 })
 
+routes.post('/instructors', (req, res) => {
+    const keys = Object.keys(req.body)
+
+    for (let key of keys) {
+        if (req.body[key] == '') 
+            res.send('Please fill and the fields!')
+        
+    }
+
+    return 
+    // return res.send(req.body)
+})
+
 routes.get('/instructors/create', (req, res) => {
     return res.render('instructors/create')
 })
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const routes = require('./routes')
 
 const server = express()
 
-
+server.use(express.urlencoded( { extended: true }))
 server.use(routes)
 server.use(express.static('public'))
 
