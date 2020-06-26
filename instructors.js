f7daf74f112cd404e3e6f76f6d10165dbdb1a5c2
Silva404 <brenosilva0252@gmail.com>
2020-06-26 13:02:12 -0300

    editing data and removing

--- instructors.js
+++ instructors.js
@@ -7,15 +7,13 @@ exports.show = (req, res) => {
     const { id } = req.params
 
     const foundInstructors = data.instructors.find(instructor => instructor.id == id)
-    if(!foundInstructors) return res.send('not found')
-
-    
+    if (!foundInstructors) return res.send('not found')
 
     const instructor = {
-        ...foundInstructors,    
+        ...foundInstructors,
         age: age(foundInstructors.birth),
         services: foundInstructors.services.split(','),
-        created_at: formatter.format(foundInstructors.created_at),     
+        created_at: formatter.format(foundInstructors.created_at),
     }
 
     return res.render('instructors/show', { instructor })
@@ -26,7 +24,7 @@ exports.edit = (req, res) => {
     const { id } = req.params
 
     const foundInstructors = data.instructors.find(instructor => instructor.id == id)
-    if(!foundInstructors) return res.send('not found')
+    if (!foundInstructors) return res.send('not found')
 
     const instructor = {
         ...foundInstructors,
@@ -39,7 +37,6 @@ exports.edit = (req, res) => {
 
 // create
 exports.post = (req, res) => {
-    // form authentication
     const keys = Object.keys(req.body)
 
     for (let key of keys) {
@@ -47,10 +44,8 @@ exports.post = (req, res) => {
             res.send('Please fill and the fields!')
     }
 
-    // deconstructing data from req.body so i can select the data that i push
     let { avatar_url, birth, gender, services, name } = req.body
 
-    // data conversion     
     birth = Date.parse(birth)
     const created_at = Date.now()
     const id = Number(data.instructors.length + 1)
@@ -60,14 +55,55 @@ exports.post = (req, res) => {
         birth,
         gender,
         id,
+        name,
         services,
         created_at
     })
 
-    // system data
     fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
         if (err) return res.send('Error occurred while writing file!')
 
-        return res.redirect('/instructors')
+        return res.redirect(`/instructors/${id}`)
     })
+}
+
+// put 
+exports.put = (req, res) => {
+    const { id } = req.body
+    let index = 0
+
+    const foundInstructors = data.instructors.find((instructor, foundIndex) => {
+        if (instructor.id == id) {
+            index = foundIndex
+            return true
+        }
+    })
+    if (!foundInstructors) return res.send('not found')
+
+    const instructor = {
+        ...foundInstructors,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), err => {
+        if (err) return res.send('Instructor not found!')
+
+        return res.redirect(`/instructors/${id}`)
+    })
+}
+
+// delete
+exports.delete = (req, res) => {
+    const { id } = req.body
+
+    const filteredInstructors = data.instructors.filter( instructor => instructor.id != id)
+
+    data.instructors = filteredInstructors
+
+    fs.writeFile("data.json",)
+
+    return res.redirect('/instructors')
 }
