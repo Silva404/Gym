f962fcd5bf4179711d493cc7a8ae57d9a79cd43e
Silva404 <brenosilva0252@gmail.com>
2020-07-02 11:47:55 -0300

    db init

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,9 +1,10 @@
 const { age, formatter, date } = require('../../lib/utils')
+const db = require('../../config/db')
 
 exports = {
     index(req, res) {
         return res.render('instructors/index')
-    },
+    },  
     create(req, res) {
         return res.render('instructors/create')
     },
@@ -14,6 +15,34 @@ exports = {
             if (req.body[key] == '')
                 res.send('Please fill and the fields!')
         }
+
+        const query = `
+            INSERT INTO instructors  (
+                name,
+                avatar_url,
+                gender,
+                services,
+                birth,
+                created_at
+            ) VALUES (1$, 2$, 3$, 4$, 5$, 6$)
+            RETURNING id
+        `
+
+        const values = [
+            req.body.name,
+            req.body.avatar_url,
+            req.body.gender,
+            req.body.services,
+            date(req.body.birth).iso,
+            date(Date.now()).iso
+        ]
+
+        db.query(query, values, (err, results) => {
+            console.log(err)
+            console.log(results)
+            return
+        })
+        
     },
     edit(req, res) {
         return
