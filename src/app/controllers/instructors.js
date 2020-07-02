300a514b4d05f10a3ec303781067a4a9cd648ca1
Silva404 <brenosilva0252@gmail.com>
2020-07-02 10:36:20 -0300

    db init

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,120 +1,35 @@
-const fs = require('fs')
-const data = require('../data.json')
-const { age, formatter, date } = require('../utilities')
+const { age, formatter, date } = require('../../lib/utils')
 
-exports.index = (req, res) => {
-    return res.render('instructors/index', { instructors: data.instructors })
-}
+exports = {
+    index(req, res) {
+        return res.render('instructors/index')
+    },
+    create(req, res) {
+        return res.render('instructors/create')
+    },
+    post(req, res) {
+        const keys = Object.keys(req.body)
 
-exports.edit = (req, res) => {
-    const { id } = req.params
-
-    const foundInstructors = data.instructors.find(instructor => instructor.id == id)
-    if (!foundInstructors) return res.send('not found')
-
-    const instructor = {
-        ...foundInstructors,
-        birth: date(foundInstructors.birth).iso
-    }
-
-
-    return res.render('instructors/edit', { instructor })
-}
-
-exports.create = (req, res) => {
-    return res.render('instructors/create')
-}
-
-exports.post = (req, res) => {
-    const keys = Object.keys(req.body)
-
-    for (let key of keys) {
-        if (req.body[key] == '')
-            res.send('Please fill and the fields!')
-    }
-
-    let { avatar_url, birth, gender, services, name } = req.body
-
-    birth = Date.parse(birth)
-    const created_at = Date.now()
-    const id = Number(data.instructors.length + 1)
-    const services_New = services.split(',')
-
-    data.instructors.push({
-        avatar_url,
-        birth,
-        gender,
-        id,
-        name,
-        services: services_New,
-        created_at
-    })
-
-    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
-        if (err) return res.send('Error occurred while writing file!')
-
-        return res.redirect(`/instructors/${id}`)
-    })
-}
-
-exports.show = (req, res) => {
-    const { id } = req.params
-
-    const foundInstructors = data.instructors.find(instructor => instructor.id == id)
-    if (!foundInstructors) return res.send('not found')
-
-    const instructor = {
-        ...foundInstructors,
-        age: age(foundInstructors.birth),
-        created_at: date(foundInstructors.created_at).created,
-    }
-
-    return res.render('instructors/show', { instructor })
-}
-
-exports.put = (req, res) => {
-    const { id } = req.body
-    let index = 0
-
-    const foundInstructors = data.instructors.find((instructor, foundIndex) => {
-        if (instructor.id == id) {
-            index = foundIndex
-            return true
+        for (let key of keys) {
+            if (req.body[key] == '')
+                res.send('Please fill and the fields!')
         }
-    })
-    if (!foundInstructors) return res.send('not found')
+    },
+    edit(req, res) {
+        return
+    },
+    show(req, res) {
+        return
+    },
+    put(req, res) {
+        const keys = Object.keys(req.body)
 
-    let { services } = req.body
-    const services_New = services.split(',')
-
-    const instructor = {
-        ...foundInstructors,
-        ...req.body,
-        birth: Date.parse(req.body.birth),
-        id: Number(req.body.id),
-        services: services_New
-    }
-
-    data.instructors[index] = instructor
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), err => {
-        if (err) return res.send('Instructor not found!')
-
-        return res.redirect(`/instructors/${id}`)
-    })
-}
-
-exports.delete = (req, res) => {
-    const { id } = req.body
-
-    const filteredInstructors = data.instructors.filter( instructor => instructor.id != id)
-
-    data.instructors = filteredInstructors
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), err => {
-        if (err) res.send('User not found')
-
-        return res.redirect('/instructors')
-    })
-
+        for (let key of keys) {
+            if (req.body[key] == '')
+                res.send('Please fill and the fields!')
+        }
+    },
+    delete(req, res) {
+        return
+    },
 }
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const routes = express.Router()
-const instructors = require('./controllers/instructors')
-const members = require('./controllers/members')
+const instructors = require('./app/controllers/instructors')
+const members = require('./app/controllers/members')
 
 routes.get('/', (req, res) => {
     res.redirect('/instructors')
