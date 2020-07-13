0015a599f848eb4da8075a6919f90665ab3f00e2
Silva404 <brenosilva0252@gmail.com>
2020-07-13 18:07:59 -0300

    algorithm paginate

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -8,3 +8,35 @@ for (let item of menuItems) {
     }
 }
 
+// duas paginas a frente e duas paginas antes
+// em casa de haver somente um pagina entre duas ultimas e a página limite
+// mostrar também esse numero sem os pontinhos
+
+
+let totalPages = 20,
+    selectedPage = 5,
+    pages = [],
+    oldPage
+
+for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+
+    if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+       if (oldPage && currentPage - oldPage > 2) {
+        pages.push('...')
+       }
+
+       if (oldPage && currentPage - oldPage == 2) {
+           pages.push(oldPage + 1)
+       }
+       
+        pages.push(currentPage)
+        oldPage = currentPage
+    }
+}
+
+console.log(pages)
